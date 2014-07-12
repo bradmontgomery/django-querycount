7e0158f581e060bab28db6d03c6f5c6edb91e482
Morgan Phillips <morgan@artistgrowth.com>
2014-07-12 11:05:49 -0500

    Fixes a bug with counting reads and adds a unit test to account for it.  Also adds myself to the setup.py but feel free to remove if you think it's not warranted.

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name="django-querycount",
     version="0.0.0",
-    author="Brad Montgomery",
-    author_email="brad@bradmontgomery.net",
+    author="Brad Montgomery, Morgan Phillips",
+    author_email="brad@bradmontgomery.net, winter2718@gmail.com",
     description=("Middleware that Prints the number of DB queries to the runserver console."),
     install_requires=[],
     license="MIT",
-    keywords="django querycount",
+    keywords="django querycount database performance",
     url="https://github.com/bradmontgomery/django-querycount/tarball/0.0.0",
     packages=[
         "querycount",
