f96115b7e92ff40c0197ed7d87543db0c62b3453
Brad Montgomery <brad@bradmontgomery.net>
2015-05-01 16:56:52 -0500

    add more classifiers and make sure the url references the correct release

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup
 from querycount import __version__
 
+url = "https://github.com/bradmontgomery/django-querycount/tarball/{0}".format(__version__)
+
 setup(
     name="django-querycount",
     version=__version__,
@@ -10,13 +12,20 @@ setup(
     install_requires=[],
     license="MIT",
     keywords="django querycount database performance",
-    url="https://github.com/bradmontgomery/django-querycount/tarball/0.0.0",
+    url=url,
     packages=[
         "querycount",
     ],
     long_description="this project gives you a middleware that"
     "prints DB query counts in Django's runserver console output.",
     classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
         "Topic :: Utilities",
     ],
 )
