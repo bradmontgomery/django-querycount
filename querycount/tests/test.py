e5d825287ffd522a2b3652b12e9ab14583ad96ab
Brad Montgomery <brad@bradmontgomery.net>
2017-04-22 18:15:03 -0500

    replaced tests with simple smoke tests that are runnable with ./manage.py test query_count

--- querycount/tests/test.py
+++ querycount/tests/test.py
@@ -1,73 +1,51 @@
-#!/usr/bin/env python
-
-from __future__ import absolute_import
-
-import os
-import sys
-
-from test_app.settings import DEFAULT_SETTINGS
-
-from django.conf import settings
-
-if settings.configured is not True:
-    settings.configure(**DEFAULT_SETTINGS)
-settings.DEBUG = True
-
-from django.test import TestCase
-
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
-from middleware import QueryCountMiddleware
-from test_app.models import TestModel
+from django.test import TestCase, modify_settings, override_settings
+from querycount.middleware import QueryCountMiddleware
 
 
+DEFAULT_QC_SETTINGS = {
+    'THRESHOLDS': {
+        'MEDIUM': 50,
+        'HIGH': 200,
+        'MIN_TIME_TO_LOG': 0,
+        'MIN_QUERY_COUNT_TO_LOG': 0
+    },
+    'IGNORE_REQUEST_PATTERNS': [],
+    'IGNORE_SQL_PATTERNS': [],
+    'DISPLAY_DUPLICATES': None,
+}
+
+QC_MIDDLEWARE = {
+    'append': 'querycount.middleware.QueryCountMiddleware',
+}
+
+
+@modify_settings(MIDDLEWARE=QC_MIDDLEWARE)
+@override_settings(QUERYCOUNT=DEFAULT_QC_SETTINGS)
+@override_settings(ROOT_URLCONF='querycount.tests.urls')
+@override_settings(DEBUG=True)
 class QueryCountTestCase(TestCase):
+    # -------------------------------------------------------------------------
+    # TODO: I want this app to be testable with just django's built-in tools,
+    #       e.g. by running `python manage.py test querycount`.
+    #
+    #   - Figure out if there's a way to access the Middleware classes from a
+    #     test client?
+    #   - How to generate DB queries (read + write) reliably without jumping
+    #     through the hoops of having a `test app` (that then needs to be
+    #     monkey-patched in)
+    #   - Should we just write a dump unit test for each of the
+    #     QueryCountMiddleware's methods (probably).
+    # -------------------------------------------------------------------------
 
-        def setUp(self):
-            self.querycount = QueryCountMiddleware()
+    def setUp(self):
+        self.querycount = QueryCountMiddleware()
 
-        def test_setup(self):
-            for name, details in DEFAULT_SETTINGS['DATABASES'].items():
-                self.assertTrue(settings.DEBUG)
-                self.assertIn(name, self.querycount.dbs)
+    def test_empty(self):
+        # Smoke test for a view that does no DB queries.
+        resp = self.client.get("/empty/")
+        self.assertEqual(resp.status_code, 200)
 
-        def get_fresh_stats(self):
-            self.querycount._reset_stats()
-            for key in self.querycount.stats.keys():
-                self.querycount._count_queries(key)
-            return self.querycount.stats
-
-        def test_query_stats(self):
-            starting_stats = self.get_fresh_stats()
-
-            # ensure that writes are adding correctly
-            model_instance = TestModel(text_field='abc').save() # NOQA
-            post_write_stats = self.get_fresh_stats()
-            for name in self.querycount.dbs:
-                for key in self.querycount.stats.keys():
-                    self.assertEqual(
-                        starting_stats[key][name]['writes'] + 1,
-                        post_write_stats[key][name]['writes']
-                    )
-                    self.assertEqual(
-                        starting_stats[key][name]['reads'],
-                        post_write_stats[key][name]['reads']
-                    )
-
-            # ensure that reads are adding correctly
-            model_instance = TestModel.objects.get(pk=1) # NOQA
-            post_read_stats = self.get_fresh_stats()
-            for name in self.querycount.dbs:
-                for key in self.querycount.stats.keys():
-                    self.assertEqual(
-                        post_write_stats[key][name]['reads'] + 1,
-                        post_read_stats[key][name]['reads']
-                    )
-                    self.assertEqual(
-                        post_write_stats[key][name]['writes'],
-                        post_read_stats[key][name]['writes']
-                    )
-
-if __name__ == '__main__':
-    from django.test.runner import DiscoverRunner
-    test_runner = DiscoverRunner(verbosity=1, interactive=True, failfast=False)
-    test_runner.run_tests(['.'])
+    def test_count(self):
+        # Smoke test for a view that does a single DB queries.
+        resp = self.client.get("/count/")
+        self.assertEqual(resp.status_code, 200)
